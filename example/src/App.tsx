d2b34652a57efd7ba882f9b7e98203e194636fa0
kiu <60544742+kiukiu328@users.noreply.github.com>
2024-02-19 20:12:31 +0800

    App.tsx for display

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,20 @@
 import * as React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-import { StyleSheet, View, Text } from 'react-native';
 import Wherami from 'react-native-wherami';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const [location, setLocation] = React.useState();
 
   React.useEffect(() => {
-    Wherami.initialize();
+    Wherami.checkPermission();
+    Wherami.start();
   }, []);
 
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
+    <View>
+      <Button title="Location" onPress={() => Wherami.location().then(setLocation)} />
+    <Text>{location}</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
-  },
-});
